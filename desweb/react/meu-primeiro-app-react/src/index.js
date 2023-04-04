19ad8bd294b730dd747989ef4c953ab0d9913b53
asbonato <asbonato@gmail.com>
2023-04-03 21:20:04 -0300

    aula 03/04 Apostila 3 de React completa - introdução

---
+++ desweb/react/meu-primeiro-app-react/src/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles.css'
+
+const App = () => {
+
+    const estilosBotao = {marginTop:12, paddingTop:8, paddingBottom:8,
+        backgroundColor:'blueviolet', color:'white', border:'none',
+        width:'100%', borderRadius:8}
+    const textoDoRotulo = 'Nome:'
+    const obterTextoDoBotao = () => {
+        return 'Enviar'
+    }
+
+    return (
+        <div style={{margin:"auto", width:768, backgroundColor: '#EEE',
+    padding:12, borderRadius:8}}>
+            <label className="rotulo" htmlFor="nome" style={{display: 'block', marginBottom:4}}>
+                {textoDoRotulo}
+            </label>
+            <input type="text" id="nome" style={{paddingTop:8, paddingBottom:8,
+            borderStyle:'hidden', width:'100%', borderRadius:8, outline:'none',
+            boxSizing:'border-box'}}/>
+            <button style={estilosBotao}>{obterTextoDoBotao()}</button>
+        </div>    
+    )
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector("#root")
+)
