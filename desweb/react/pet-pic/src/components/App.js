171456dc9a6daf83121e5cf1afc109d7b5039d7b
Rodrigo Bossini <professorbossini@gmail.com>
2023-04-25 20:06:27 -0300

    apostila07(pet-pic): terminando a secao 2.3

---
+++ desweb/react/pet-pic/src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+export default () => {
+  return (
+    <div>
+      <h1>
+        Exibir uma lista de...
+      </h1>
+    </div>
+  )
+}
---
+++ desweb/react/pet-pic/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+import 'primereact/resources/primereact.min.css'
+import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.min.css'
+import 'primereact/resources/themes/bootstrap4-light-blue/theme.css'
+
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+)
