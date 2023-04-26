91112e7d70c524250bc1dc3481ed37fe002ae866
Rodrigo Bossini <professorbossini@gmail.com>
2023-04-25 21:44:19 -0300

    apostila07(pet-pic): fim da secao 2.4

--- desweb/react/pet-pic/src/components/App.js
+++ desweb/react/pet-pic/src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Busca from './Busca'
 export default () => {
   return (
     <div>
       <h1>
         Exibir uma lista de...
       </h1>
+      <Busca />
     </div>
   )
 }
---
+++ desweb/react/pet-pic/src/components/Busca.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import { InputText } from 'primereact/inputtext'
+import { Button } from 'primereact/button'
+export default class Busca extends Component{
+  state = {
+    termoDeBusca: ''
+  }
+
+  onTermoAlterado = (event) => {
+    // console.log(event.target.value)
+    this.setState({termoDeBusca: event.target.value})
+  }
+
+  render(){
+    return (
+      <div className='flex flex-column'>
+        {/* span.p-input-icon-left.w-full>i.pi.pi-search */}
+        <span className="p-input-icon-left w-full">
+          <i className="pi pi-search"></i>
+          <InputText
+            value={this.state.termoDeBusca} 
+            className="w-full"
+            onChange={this.onTermoAlterado}
+            placeholder={this.props.dica}
+          />
+        </span>
+        <Button 
+          label='OK'
+          className='p-button-outlined mt-2'
+        />
+      </div>
+    )
+  }
+}
+
+Busca.defaultProps = {
+  dica: 'Digite algo que deseja ver...'
+}
