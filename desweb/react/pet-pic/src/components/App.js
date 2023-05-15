db002dd3f8a230544238fafff6b1abbe05a28ead
asbonato <asbonato@gmail.com>
2023-05-15 20:49:58 -0300

    aula 15-05 - react - apostila 7 finalizada

--- desweb/react/pet-pic/src/components/App.js
+++ desweb/react/pet-pic/src/components/App.js
@@ -1,26 +1,40 @@
 import React from 'react'
 import Busca from './Busca'
-import env from 'react-dotenv'
-import { createClient } from 'pexels'
+//import env from 'react-dotenv'
+//import { createClient } from 'pexels'
 import ListaImagens from './ListaImagens'
 import PexelsLogo from './PexelsLogo'
+import pexelsClient from '../utils/pexelsClient'
 
 export default class App extends React.Component {
 
-  pexelsClient = null
+  //pexelsClient = null
 
   state = {pics: []}
 
-  componentDidMount(){
-    this.pexelsClient = createClient(env.PEXELS_KEY)
-  }
+  // componentDidMount(){
+  //   this.pexelsClient = createClient(env.PEXELS_KEY)
+  // }
+
+  // onBuscaRealizada = (termo) => {
+  //   this.pexelsClient.photos.search({
+  //     query: termo
+  //   })
+  //   .then(pics => this.setState({pics: pics.photos}))
+  // }
 
   onBuscaRealizada = (termo) => {
-    this.pexelsClient.photos.search({
-      query: termo
+    pexelsClient.get('/search', {
+      params: { query: termo}
     })
-    .then(pics => this.setState({pics: pics.photos}))
+    .then(result => {
+      console.log(result)
+      //data é um atributo definido pela axios
+      //o conteúdo da resposta vem associado a esta chave
+      this.setState({pics: result.data.photos})
+    })
   }
+
   render(){
     return (
       <div className='grid justify-content-center m-auto w-9 border-round border-1 border-400'>
@@ -32,11 +46,15 @@ export default class App extends React.Component {
             Exibir uma lista de...
           </h1>
         </div>
-        <div className='col-8'>
+        <div className='col-12'>
           <Busca onBuscaRealizada={this.onBuscaRealizada}/>
         </div>
-        <div className='col-8'>
-          <ListaImagens pics={this.state.pics}/>
+        <div className='col-12'>
+          <div className='grid'>
+            <ListaImagens
+              imgStyle={'col-12 md:col-6 lg:col-4 xl:col-3'} 
+              pics={this.state.pics}/>
+          </div>
         </div>
       </div>
     )
