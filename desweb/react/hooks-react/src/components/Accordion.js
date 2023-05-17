f1b089f6df56a493bf331dacdaf187d3be36850a
Rodrigo Bossini <professorbossini@gmail.com>
2023-05-16 21:41:20 -0300

    apostila08(hooks): fim da secao 2.11

--- desweb/react/hooks-react/src/components/Accordion.js
+++ desweb/react/hooks-react/src/components/Accordion.js
@@ -1,16 +1,28 @@
-import React from "react"
+import React, { useState } from "react"
+import { Card } from 'primereact/card'
+import './Accordion.css'
 
 const Accordion = ({itens}) => {
+    const [indiceAtivo, setIndiceAtivo] = useState(null)
+    const itemClicado = (indice) => {
+        setIndiceAtivo(indice)
+    }
+    const expressaoJSX = itens.map((item, indice) =>{
+        const classExibirConteudo = indice === indiceAtivo ? '' : 'hidden'
+        const classExibirIcone = indice === indiceAtivo ? 'pi-angle-down' : 'pi-angle-right'
+        return(
+            <Card id="accordion" key={indice} className="border-1 border-400">
+                <div onClick={() => itemClicado(indice)}>
+                    <i className={`pi ${classExibirIcone}`}></i>
+                    <h5 className="inline ml-3">{item.titulo}</h5>
+                </div>
+                <p className={classExibirConteudo}>{item.conteudo}</p>
+            </Card>
+        )
+    })
     return (
         <div>
-            {
-                itens.map((item, indice) => (
-                    <div key={indice}>
-                        <h4>{item.titulo}</h4>
-                        <p>{item.conteudo}</p>
-                    </div>
-                ))
-            }
+            {expressaoJSX}
         </div>
     )
 }
